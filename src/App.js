478e722cd751a2cabfd3b83edebefc6b313a0063
Abdullah <abdullahr797@gmail.com>
2022-08-28 18:44:10 +0500

    class 3 part 2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
-
+import Message from './Message';
 function App() {
+  const [count,setCount] = useState(0)
+  const[morning,setMorning] = useState(false)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+        <div className={`box ${morning ?"dayLight": " "} `}>
+        <h1>Hava a Good {morning?"MORNING":"NIGHT"}</h1>
+      <Message counter={count}/>
+      
+
+      <button onClick={()=>{setCount(count + 1)}}>Change</button>
+
+      <button onClick={()=>{setMorning(!morning)}}> weather</button>
+    </div>
     </div>
   );
 }
---
+++ src/Message.jsx
@@ -0,0 +1,9 @@
+import React, { useState } from 'react'
+
+export default function Message(props) {
+
+  return (
+  
+   <h3>The Value of counter is {props.counter}</h3>
+  )
+}
